b3749aae83bae3251828303887017ad994cb68a2
mfsaglam <mfsaglam@yandex.com>
2022-10-23 18:41:11 +0300

    use euclid formula

--- AspectRatio/AspectRatioCalculator.swift
+++ AspectRatio/AspectRatioCalculator.swift
@@ -27,14 +27,28 @@ struct AspectRatioCalculator {
             return String(format: "%.2f", result) + ":1"
             
         case .widthAndHeightBased:
-            let gcd = gcd(width, height)
+            let gcd = gcdIterativeEuklid(width, height)
             return String(format: "%.0f", width / gcd) + ":" + String(format: "%.0f", height / gcd)
         }
     }
     
+    func gcdIterativeEuklid(_ m: Float, _ n: Float) -> Float {
+        var a: Float = 0
+        var b: Float = max(m, n)
+        var r: Float = min(m, n)
+
+        while r != 0 {
+            a = b
+            b = r
+            r = a.truncatingRemainder(dividingBy: b)
+//            r = a % b
+        }
+        return b
+    }
+    
     private func gcd(_ a: Float, _ b: Float) -> Float {
         let r = a.truncatingRemainder(dividingBy: b)
-        if r != 0 {
+        if r > 10 {
             return gcd(b, r)
         } else {
             return b
