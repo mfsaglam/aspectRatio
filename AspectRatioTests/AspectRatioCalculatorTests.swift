d973397b1f303005763f34c85b782825a4a19ada
mfsaglam <mfsaglam@yandex.com>
2022-11-11 23:01:46 +0300

    write more tests

--- AspectRatioTests/AspectRatioCalculatorTests.swift
+++ AspectRatioTests/AspectRatioCalculatorTests.swift
@@ -18,12 +18,28 @@ final class AspectRatioCalculatorTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func test_Calculate_1920by1080is16by9() {
+    func test_Calculate_wh_1920by1080is16by9() {
         let sut = AspectRatioCalculator()
         
         let result = sut.calculate(width: 1920, height: 1080, format: .widthAndHeightBased)
         
         XCTAssertEqual(result, "16:9")
+    }
+    
+    func test_Calculate_one_1920by1080is178by1() {
+        let sut = AspectRatioCalculator()
+        
+        let result = sut.calculate(width: 1920, height: 1080, format: .oneBased)
+        
+        XCTAssertEqual(result, "1.78:1")
+    }
+    
+    func test_Calculate_diagonal_1920by1080is2202() {
+        let sut = AspectRatioCalculator()
+        
+        let result = sut.calculate(width: 1920, height: 1080, format: .diagonal)
+        
+        XCTAssertEqual(result, "2202.91")
     }
 
 }
