ae646f05a1eb477592e2fe0be8c351f8a95b922a
mfsaglam <mfsaglam@yandex.com>
2022-11-11 22:47:03 +0300

    add tests bundle

---
+++ AspectRatioTests/AspectRatioTests.swift
@@ -0,0 +1,20 @@
+//
+//  AspectRatioTests.swift
+//  AspectRatioTests
+//
+//  Created by Fatih Sağlam on 11.11.2022.
+//
+
+import XCTest
+
+final class AspectRatioTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
